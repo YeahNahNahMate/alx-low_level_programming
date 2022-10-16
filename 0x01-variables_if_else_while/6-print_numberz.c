fa2e33638adf6f8620b48a7c0bdeac76c59f0296
YeahNhNahMate <macleverous@outlook.com>
2022-10-16 15:53:39 -0700

    Prints zero to nine

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	int num;
 
-	for (num = 0 ; num <= 9 ; num++)
+	for (num = '0' ; num <= '9' ; num++)
 	{
-		putchar(num);
+		putchar((num % 10) + '0');
 	}
 	putchar("\n");
 	return (0);
