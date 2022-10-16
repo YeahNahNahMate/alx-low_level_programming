5220bf912b7748d3d59f025afe1aad913e96093a
YeahNhNahMate <macleverous@outlook.com>
2022-10-16 11:31:16 -0700

    Prints text and stderror

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a text to stadard error.
+ *
+ * Return: 1 to end program
+ */
+
+int main(void)
+{
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19		\n";
+
+	fprintf(stderr, "%s\n", n);
+
+	return (1);
+}
