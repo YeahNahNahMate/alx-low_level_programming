a4e873eb7d112ff2ef9d9b2483e575c88ad92155
YeahNhNahMate <macleverous@outlook.com>
2022-10-19 13:46:09 -0700

    still not compilling

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,11 +11,11 @@
 int main(void)
 {
 	int i;
-	char wrd[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	char wrd[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 
-	for (i = 0 ; i < 9 ; i++)
+	for (i = 0 ; i < 8 ; i++)
 	{
 		putchar(wrd[i]);
 	}
 	return (0);
-}
+}i
